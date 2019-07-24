4eab0b28311517f200297248ec040fe09d2d71c9
liyihang <lidoudoou@gmail.com>
2019-07-24 09:11:35 +0800

    some animation

--- src/components/courseDetail.vue
+++ src/components/courseDetail.vue
@@ -13,10 +13,30 @@
     <!-- 课程详情 -->
     <el-container>
       <el-row :gutter="24" style="margin:0 auto" justify="space-around">
-        <el-divider content-position="left">基础练习</el-divider>
+        <el-divider content-position="left" class="easing-variables">基础练习</el-divider>
         <el-col :span="8">
           <el-card shadow="always">
-            <h4>HTML</h4>
+            <h4 class="easing-variables">爱的魔力转圈圈</h4>
+            <p class="hover-underline-animation">
+              <el-link icon="el-icon-edit">HTML标签和JavaScript应用12678wefvsdfsdfaf</el-link>
+            </p>
+            <p>
+              <el-link icon="el-icon-edit">HTML标签和JavaScript应用</el-link>
+            </p>
+            <p>
+              <el-link icon="el-icon-edit">HTML标签和JavaScript应用</el-link>
+            </p>
+            <p>
+              <el-link icon="el-icon-edit">HTML标签和JavaScript应用</el-link>
+            </p>
+            <p>
+              <el-link icon="el-icon-edit">HTML标签和JavaScript应用</el-link>
+            </p>
+          </el-card>
+        </el-col>
+        <el-col :span="8">
+          <el-card shadow="always">
+            <h4 class="easing-variables">HTML</h4>
             <p>
               <el-link icon="el-icon-edit">HTML标签和JavaScript应用</el-link>
             </p>
@@ -36,27 +56,7 @@
         </el-col>
         <el-col :span="8">
           <el-card shadow="always">
-            <h4>HTML</h4>
-            <p>
-              <el-link icon="el-icon-edit">HTML标签和JavaScript应用</el-link>
-            </p>
-            <p>
-              <el-link icon="el-icon-edit">HTML标签和JavaScript应用</el-link>
-            </p>
-            <p>
-              <el-link icon="el-icon-edit">HTML标签和JavaScript应用</el-link>
-            </p>
-            <p>
-              <el-link icon="el-icon-edit">HTML标签和JavaScript应用</el-link>
-            </p>
-            <p>
-              <el-link icon="el-icon-edit">HTML标签和JavaScript应用</el-link>
-            </p>
-          </el-card>
-        </el-col>
-        <el-col :span="8">
-          <el-card shadow="always">
-            <h4>HTML</h4>
+            <h4 class="easing-variables">HTML</h4>
             <p>
               <el-link icon="el-icon-edit">HTML标签和JavaScript应用</el-link>
             </p>
@@ -80,7 +80,7 @@
     </el-container>
     <el-container>
       <el-row :gutter="24" style="margin:0 auto" justify="space-around">
-        <el-divider content-position="left">基础练习</el-divider>
+        <el-divider content-position="left" >编程练习</el-divider>
         <el-col :span="8">
           <el-card shadow="always">
             <h4>HTML</h4>
@@ -147,11 +147,11 @@
     </el-container>
     <el-container>
       <el-row :gutter="24" style="margin:0 auto" justify="space-around">
-        <el-divider content-position="left">基础练习</el-divider>
+        <el-divider content-position="left" >综合练习</el-divider>
         <el-col :span="8">
           <el-card shadow="always">
             <h4>HTML</h4>
-            <p class="hover-underline-animation">
+            <p >
               <el-link icon="el-icon-edit" @click="getExam(1)">HTML标签和JavaScript应用123</el-link>
             </p>
             <p>
@@ -250,6 +250,9 @@ export default {
 }
 </script>
 <style scoped>
+.el-col-8{
+  width:28rem;
+}
 .el-container {
   padding: 2rem;
 }
@@ -286,5 +289,40 @@ export default {
 .hover-underline-animation:hover::after {
   transform: scaleX(1);
   transform-origin: bottom left;
+}
+:root {
+  /* Place variables in here to use globally */
+}
+.easing-variables {
+  --ease-in-quad: cubic-bezier(0.55, 0.085, 0.68, 0.53);
+  --ease-in-cubic: cubic-bezier(0.55, 0.055, 0.675, 0.19);
+  --ease-in-quart: cubic-bezier(0.895, 0.03, 0.685, 0.22);
+  --ease-in-quint: cubic-bezier(0.755, 0.05, 0.855, 0.06);
+  --ease-in-expo: cubic-bezier(0.95, 0.05, 0.795, 0.035);
+  --ease-in-circ: cubic-bezier(0.6, 0.04, 0.98, 0.335);
+  --ease-out-quad: cubic-bezier(0.25, 0.46, 0.45, 0.94);
+  --ease-out-cubic: cubic-bezier(0.215, 0.61, 0.355, 1);
+  --ease-out-quart: cubic-bezier(0.165, 0.84, 0.44, 1);
+  --ease-out-quint: cubic-bezier(0.23, 1, 0.32, 1);
+  --ease-out-expo: cubic-bezier(0.19, 1, 0.22, 1);
+  --ease-out-circ: cubic-bezier(0.075, 0.82, 0.165, 1);
+  --ease-in-out-quad: cubic-bezier(0.455, 0.03, 0.515, 0.955);
+  --ease-in-out-cubic: cubic-bezier(0.645, 0.045, 0.355, 1);
+  --ease-in-out-quart: cubic-bezier(0.77, 0, 0.175, 1);
+  --ease-in-out-quint: cubic-bezier(0.86, 0, 0.07, 1);
+  --ease-in-out-expo: cubic-bezier(1, 0, 0, 1);
+  --ease-in-out-circ: cubic-bezier(0.785, 0.135, 0.15, 0.86);
+  display: inline-block;
+  width: 15rem;
+  height: 2rem;
+  color: black;
+  text-align: center;
+  transition: transform 1s var(--ease-out-quart);
+}
+.easing-variables:hover {
+  transform: rotate(1280000deg);
+}
+h4{
+  margin:0;
 }
 </style>
